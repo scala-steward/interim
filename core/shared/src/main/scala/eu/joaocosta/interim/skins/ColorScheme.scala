ff5db6d485eee8ef9c20152c9f7a25a5fc1190fd
João Costa <jdpc557@gmail.com>
2026-08-23 21:31:41 +0100

    Use ImGui inspired color schemes

--- core/shared/src/main/scala/eu/joaocosta/interim/skins/ColorScheme.scala
+++ core/shared/src/main/scala/eu/joaocosta/interim/skins/ColorScheme.scala
@@ -15,21 +15,25 @@ final case class ColorScheme(
     borderColor: Color         // pureGray / pureGray
 )
 
-/** Internal default color scheme used by InterIm's default skins */
+/** Internal default color scheme used by InterIm's default skins
+  * Inspired by:
+  *   - https://github.com/raysan5/raygui/tree/master/styles/default
+  *   - https://github.com/raysan5/raygui/tree/master/styles/genesis
+  */
 object ColorScheme:
-  val white     = Color(246, 247, 251)
-  val lightGray = Color(177, 186, 177)
+  val white     = Color(245, 245, 245)
+  val lightGray = Color(201, 201, 201)
   val pureGray  = Color(127, 127, 127)
-  val darkGray  = Color(77, 77, 77)
-  val black     = Color(23, 21, 23)
+  val darkGray  = Color(46, 53, 51)
+  val black     = Color(24, 27, 30)
 
-  val lightPrimary          = Color(9, 211, 222)
-  val lightPrimaryShadow    = Color(15, 172, 186)
-  val lightPrimaryHighlight = Color(0, 247, 255)
+  val lightPrimary          = Color(151, 232, 255)
+  val lightPrimaryShadow    = Color(91, 178, 217)
+  val lightPrimaryHighlight = Color(201, 239, 255)
 
-  val darkPrimary          = Color(97, 31, 125)
-  val darkPrimaryShadow    = Color(67, 24, 92)
-  val darkPrimaryHighlight = Color(130, 38, 158)
+  val darkPrimary          = Color(172, 60, 60)
+  val darkPrimaryShadow    = Color(95, 36, 36)
+  val darkPrimaryHighlight = Color(204, 118, 118)
 
   val lightScheme = ColorScheme(
     background = white,
